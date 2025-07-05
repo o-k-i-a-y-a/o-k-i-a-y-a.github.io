710d4e5767520a0c0d4245b4708b5cbbb4184ad9
o-k-i-a-y-a <hanon_023@yahoo.co.jp>
2025-07-05 17:54:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const panzoomEl = document.getElementById('panzoom');
 const instance = panzoom(panzoomEl, {
   bounds: true,
-  boundsPadding: 0.05,
+  boundsPadding: 0.3,
   maxZoom: 3,
   minZoom: 0.5,
   initialZoom: 0.7,
