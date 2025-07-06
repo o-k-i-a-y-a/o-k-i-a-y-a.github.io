1d369df15ff0d80e86c6249746d68b0b420192b2
o-k-i-a-y-a <hanon_023@yahoo.co.jp>
2025-07-06 09:02:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const panzoomEl = document.getElementById('panzoom');
-const instance = panzoom(panzoomEl, {
-  bounds: true,
-  boundsPadding: 1,//0.3,
-  maxZoom: 3,
-  minZoom: 0.5,
-  initialZoom: 0.7,
-});
+window.addEventListener("DOMContentLoaded", () => {
+  const panzoomEl = document.getElementById('image'); // ← ここをimageに
+  const instance = panzoom(panzoomEl, {
+    maxZoom: 3,
+    minZoom: 0.5,
+    initialZoom: 0.5,
+  });
 
-// 地図を初期表示で中央寄せ
-const imgEl = panzoomEl.getElementsByTagName('img')[0];
-const image = new Image();
-image.onload = function () {
-  const dw = panzoomEl.clientWidth / this.naturalWidth;
-  const dh = panzoomEl.clientHeight / this.naturalHeight;
-  const scale = Math.min(dw, dh);
-  instance.zoomAbs(
-    panzoomEl.clientWidth / 2,
-    panzoomEl.clientHeight / 2,
-    scale
-  );
-};
-image.src = imgEl.src;
+  //地図の初期位置を中央に
+  const imgEl = panzoomEl.querySelector('img');
+  const image = new Image();
+  image.onload = function () {
+    const dw = panzoomEl.clientWidth / this.naturalWidth;
+    const dh = panzoomEl.clientHeight / this.naturalHeight;
+    const scale = Math.min(dw, dh);
+    instance.zoomAbs(
+      panzoomEl.clientWidth / 2,
+      panzoomEl.clientHeight / 2,
+      scale
+    );
+  };
+  image.src = imgEl.src;
+});
